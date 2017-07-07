f24a9927a2d64be78509f52c76525277e6e83743
Jack Corrigan <jcorrigan14@gmail.com>
2017-07-07 14:35:26 -0400

    Used oauth2 Application to access client secret and client ID based on request

--- DOT_tutorial/admin.py
+++ DOT_tutorial/admin.py
@@ -5,14 +5,9 @@ from DOT_tutorial.models import *
 from guardian.admin import GuardedModelAdmin
 
 
-class ApplicationAdmin(GuardedModelAdmin):
-    pass
-admin.site.register(Application, ApplicationAdmin)
-
-
 class ApplicationGroupAdmin(GuardedModelAdmin): # Admin.ModelAdmin or GuardedModelAdmin
     pass
-admin.site.register(ApplicationGroup, ApplicationAdmin)
+admin.site.register(ApplicationGroup, ApplicationGroupAdmin)
 
 
 
